35a8f1be054b80cdae1fb2b7af9e49721608d714
Alona-Suprun <a.alonka.l@gmail.com>
2021-07-19 01:22:31 +0300

    FriendList-component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Profile from './components/Profile/Profile.js';
 import StatisticsList from './components/Statistics/StatisticsList.js';
+import FriendList from './components/FriendList/FriendList';
 import statisticalData from './statistical-data.json';
 import user from './user.json';
+import friends from './friends.json';
 
 export default function App() {
   return (
@@ -14,6 +16,7 @@ export default function App() {
         stats={user.stats}
       />
       <StatisticsList items={statisticalData} />
+      <FriendList friends={friends} />
     </div>
   );
 }
